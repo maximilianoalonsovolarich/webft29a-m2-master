0c1b622a6ae7bd1f25232f3c17396628b7f28615
Laura Coló <84424239+lauracolof@users.noreply.github.com>
2022-08-26 21:44:19 -0300

    Delete 06-React-Intro directory

--- 06-React-Intro/demo/demo-webpack/src/SaludoFuncional.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function SaludoFuncional(props) {
-  const saludo = props.lang === 'es' ? 'Hola' : 'Hello'
-  return (
-    <h1>{saludo}! {props.nombre}</h1>
-  )
-};
